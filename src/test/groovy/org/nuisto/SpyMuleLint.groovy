f8609ce20388a2749f28a4e64b1d76e5b7d9ffb1
Mehdi Bouheddi <mbouheddi@mbouheddi-ltm.internal.salesforce.com>
2019-10-22 15:52:16 +0200

    changed void to int in SpyMuleLint.Groovy

--- src/test/groovy/org/nuisto/SpyMuleLint.groovy
+++ src/test/groovy/org/nuisto/SpyMuleLint.groovy
@@ -5,7 +5,7 @@ import org.nuisto.model.OptionsModel
 class SpyMuleLint extends MuleLint {
   OptionsModel modelUsed
 
-  void runWithModel(OptionsModel model) {
+  int runWithModel(OptionsModel model) {
     this.modelUsed = model
   }
 }
